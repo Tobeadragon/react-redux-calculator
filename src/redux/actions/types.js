6b3eea79531b0c3c3f8e90c639e1bcf33a43549c
Tobeadragon <mastertyj@hotmail.com>
2021-01-17 02:47:29 +0100

    first commit

---
+++ src/redux/actions/types.js
@@ -0,0 +1,13 @@
+// When number clicked
+export const INPUT_NUMBER='INPUT_NUMBER';
+
+// When + - * / is clicked
+export const PLUS = 'PLUS';
+export const MINUS = 'MINUS';
+export const MULTIPLY ='MULTIPLY';
+export const DIVIDE = 'DIVIDE';
+
+// When = is clicked
+export const EQUAL = 'EQUAL';
+// When AC is clicked
+export const CLEAR = 'CLEAR';
